75d5dde06d9c340eb9e737d2a391a6d2052a9d2b
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-10-20 13:46:34 +0800

    refine code

--- src/code0114.rs
+++ src/code0114.rs
@@ -25,22 +25,22 @@ impl TreeNode {
         if v.is_empty() {
             return None;
         }
-        let root = Some(Rc::new(RefCell::new(TreeNode::new(v[0].unwrap()))));
+        let root = Some(Rc::new(RefCell::new(TreeNode::new(v[0]?))));
         let mut queue = std::collections::VecDeque::new();
         queue.push_back(root.clone());
         let mut i = 1;
         while i < v.len() {
-            let node = queue.pop_front().unwrap();
-            if let Some(val) = v[i] {
-                node.as_ref()?.borrow_mut().left = Some(Rc::new(RefCell::new(TreeNode::new(val))));
+            let node = queue.pop_front()?;
+            if let Some(val) = v.get(i)? {
+                node.as_ref()?.borrow_mut().left = Some(Rc::new(RefCell::new(TreeNode::new(*val))));
                 queue.push_back(node.as_ref()?.borrow().left.clone());
             }
             i += 1;
             if i >= v.len() {
                 break;
             }
-            if let Some(val) = v[i] {
-                node.as_ref()?.borrow_mut().right = Some(Rc::new(RefCell::new(TreeNode::new(val))));
+            if let Some(val) = v.get(i)? {
+                node.as_ref()?.borrow_mut().right = Some(Rc::new(RefCell::new(TreeNode::new(*val))));
                 queue.push_back(node.as_ref()?.borrow().right.clone());
             }
             i += 1;
