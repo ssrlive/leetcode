cf392f8df026b0c009b20f9fb2e29f3daab66984
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-10-20 16:03:04 +0800

    inorder_traversal in binary tree

--- src/code0116.rs
+++ src/code0116.rs
@@ -50,6 +50,23 @@ impl TreeNode {
         }
         root
     }
+
+    pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        fn helper(node: &Option<Rc<RefCell<TreeNode>>>, ret: &mut Vec<i32>) {
+            if let Some(v) = node {
+                let v = v.borrow();
+                helper(&v.left, ret);
+                ret.push(v.val);
+                helper(&v.right, ret);
+            }
+        }
+
+        let mut ret = vec![];
+        if let Some(v) = root {
+            helper(&Some(v), &mut ret);
+        }
+        ret
+    }
 }
 
 struct Solution {}
