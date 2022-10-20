6d23a2b888aeee47e3c01b5f679e532c448f6b78
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-10-20 15:51:04 +0800

    116. Populating Next Right Pointers in Each Node

--- src/code0116.rs
+++ src/code0116.rs
@@ -1,7 +1,7 @@
 #![allow(dead_code)]
 
-// 114. Flatten Binary Tree to Linked List
-// https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+// 116. Populating Next Right Pointers in Each Node
+// https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
 
 // Definition for a binary tree node.
 #[derive(PartialEq, Eq, Clone, Debug)]
@@ -9,6 +9,7 @@ pub struct TreeNode {
     pub val: i32,
     pub left: Option<Rc<RefCell<TreeNode>>>,
     pub right: Option<Rc<RefCell<TreeNode>>>,
+    pub next: Option<Rc<RefCell<TreeNode>>>,
 }
 
 impl TreeNode {
@@ -18,6 +19,7 @@ impl TreeNode {
             val,
             left: None,
             right: None,
+            next: None,
         }
     }
 
@@ -50,51 +52,43 @@ impl TreeNode {
     }
 }
 
-struct Solution;
+struct Solution {}
 
 use std::cell::RefCell;
 use std::rc::Rc;
 impl Solution {
-    pub fn flatten(root: &mut Option<Rc<RefCell<TreeNode>>>) {
-        // .flatten() takes care of the edge case of an empty root,
-        // in which case the stack will be empty.
-        let mut stack = std::iter::once(root.clone()).flatten().collect::<Vec<_>>();
-        // Create a dummy node to make it easier to append nodes without
-        // dealing with the edge case of the first node
-        let mut head = Rc::new(RefCell::new(TreeNode::new(0)));
-        while let Some(node) = stack.pop() {
-            let mut node_ref = node.borrow_mut();
-            // Push right node onto stack first, since we want to pop it last
-            if let Some(right) = node_ref.right.take() {
-                stack.push(right);
+    pub fn connect(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+        let mut cur = root.clone();
+        while let Some(cur_rc) = cur.clone() {
+            let l = cur_rc.borrow().left.clone();
+            while let Some(cur_rc) = cur {
+                if let Some(left) = cur_rc.borrow().left.clone() {
+                    left.borrow_mut().next = cur_rc.borrow().right.clone();
+                    if let Some(next) = cur_rc.borrow().next.clone() {
+                        cur_rc.borrow().right.as_ref()?.borrow_mut().next =
+                            next.borrow().left.clone();
+                    }
+                }
+                cur = cur_rc.borrow().next.clone();
             }
-            if let Some(left) = node_ref.left.take() {
-                stack.push(left);
-            }
-            // Drop node_ref to be able to move node in "head = node;"
-            drop(node_ref);
-            head.borrow_mut().right = Some(node.clone());
-            head = node;
+            cur = l;
         }
+        root
     }
 }
 
 #[test]
 fn test() {
-    let mut root = TreeNode::from_vec(vec![
+    let root = TreeNode::from_vec(vec![
         Some(1),
         Some(2),
-        Some(5),
         Some(3),
         Some(4),
-        None,
+        Some(5),
         Some(6),
+        Some(7),
     ]);
-    Solution::flatten(&mut root);
-    let mut v = vec![];
-    while let Some(node) = root {
-        v.push(node.borrow().val);
-        root = node.borrow_mut().right.take();
-    }
-    assert_eq!(v, vec![1, 2, 3, 4, 5, 6]);
+    let root = Solution::connect(root);
+
+    println!("{:#?}", root.unwrap().borrow());
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ mod code0086;
 mod code0113;
 mod code0114;
 mod code0115;
+mod code0116;
 
 fn main() {}
