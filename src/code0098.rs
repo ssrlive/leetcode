693140a8974c3fbc0c70ab3222f05ad5c0d8ddba
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-10-27 13:28:52 +0800

    98. Validate Binary Search Tree

---
+++ src/code0098.rs
@@ -0,0 +1,53 @@
+#![allow(dead_code)]
+
+// 98. Validate Binary Search Tree
+// https://leetcode.com/problems/validate-binary-search-tree/
+//
+// Given a binary tree, determine if it is a valid binary search tree (BST).
+//
+// A valid BST is defined as follows:
+//
+// - The left subtree of a node contains only nodes with keys less than the node's key.
+// - The right subtree of a node contains only nodes with keys greater than the node's key.
+// - Both the left and right subtrees must also be binary search trees.
+
+use crate::treenode::TreeNode;
+
+struct Solution;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn is_valid_bst(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        fn validate(
+            n: Option<Rc<RefCell<TreeNode>>>,
+            min: Option<Rc<RefCell<TreeNode>>>,
+            max: Option<Rc<RefCell<TreeNode>>>,
+        ) -> bool {
+            match n {
+                None => true,
+                Some(n) => {
+                    if let Some(max) = max.clone() {
+                        if max.borrow().val <= n.borrow().val {
+                            return false;
+                        }
+                    }
+                    if let Some(min) = min.clone() {
+                        if min.borrow().val >= n.borrow().val {
+                            return false;
+                        }
+                    }
+                    validate(n.borrow().left.clone(), min, Some(n.clone()))
+                        && validate(n.borrow().right.clone(), Some(n.clone()), max)
+                }
+            }
+        }
+        validate(root, None, None)
+    }
+}
+
+#[test]
+fn test() {
+    let tree = TreeNode::from_vec(&[Some(2), Some(1), Some(3)]);
+    assert_eq!(Solution::is_valid_bst(tree), true);
+}
--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ mod code0086;
 mod code0092;
 mod code0094;
 mod code0095;
+mod code0098;
 mod code0113;
 mod code0114;
 mod code0115;
