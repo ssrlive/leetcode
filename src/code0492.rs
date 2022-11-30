51d37e29a26acecbb4aab889a54de8f702a734b7
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-11-30 19:06:59 +0800

    492. Construct the Rectangle

---
+++ src/code0492.rs
@@ -0,0 +1,60 @@
+#![allow(dead_code)]
+
+// 492. Construct the Rectangle
+// https://leetcode.com/problems/construct-the-rectangle/
+//
+// A web developer needs to know how to design a web page's size. So, given a specific rectangular web page’s area, your job by now is to design a rectangular web page, whose length L and width W satisfy the following requirements:
+//
+// The area of the rectangular web page you designed must equal to the given target area.
+// The width W should not be larger than the length L, which means L >= W.
+// The difference between length L and width W should be as small as possible.
+// Return an array [L, W] where L and W are the length and width of the web page you designed in sequence.
+//
+// Example 1:
+//
+// Input: area = 4
+// Output: [2,2]
+// Explanation: The target area is 4, and all the possible ways to construct it are [1,4], [2,2], [4,1].
+// But according to requirement 2, [1,4] is illegal; according to requirement 3,  [4,1] is not optimal compared to [2,2]. So the length L is 2, and the width W is 2.
+//
+// Example 2:
+//
+// Input: area = 37
+// Output: [37,1]
+//
+// Example 3:
+//
+// Input: area = 122122
+// Output: [427,286]
+//
+// Constraints:
+//
+// - 1 <= area <= 107
+//
+
+struct Solution;
+
+impl Solution {
+    pub fn construct_rectangle(area: i32) -> Vec<i32> {
+        let mut w = (area as f64).sqrt() as i32;
+        while area % w != 0 {
+            w -= 1;
+        }
+        vec![area / w, w]
+    }
+}
+
+#[test]
+fn test_construct_rectangle() {
+    let area = 4;
+    let result = Solution::construct_rectangle(area);
+    assert_eq!(result, vec![2, 2]);
+
+    let area = 37;
+    let result = Solution::construct_rectangle(area);
+    assert_eq!(result, vec![37, 1]);
+
+    let area = 122122;
+    let result = Solution::construct_rectangle(area);
+    assert_eq!(result, vec![427, 286]);
+}
