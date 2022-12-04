a0e1ebe802c903ab247d59f9892b8cff004c94e9
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-12-04 16:20:05 +0800

    minor.

--- src/code0630.rs
+++ src/code0630.rs
@@ -16,11 +16,11 @@
 //
 // Input: courses = [[100,200],[200,1300],[1000,1250],[2000,3200]]
 // Output: 3
-// Explanation: 
+// Explanation:
 // There are totally 4 courses, but you can take 3 courses at most:
 // First, take the 1st course, it costs 100 days so you will finish it on the 100th day, and ready to take the next course on the 101st day.
-// Second, take the 3rd course, it costs 1000 days so you will finish it on the 1100th day, and ready to take the next course on the 1101st day. 
-// Third, take the 2nd course, it costs 200 days so you will finish it on the 1300th day. 
+// Second, take the 3rd course, it costs 1000 days so you will finish it on the 1100th day, and ready to take the next course on the 1101st day.
+// Third, take the 2nd course, it costs 200 days so you will finish it on the 1300th day.
 // The 4th course cannot be taken now, since you will finish it on the 3300th day, which exceeds the closed date.
 //
 // Example 2:
@@ -65,7 +65,15 @@ impl Solution {
 
 #[test]
 fn test() {
-    assert_eq!(Solution::schedule_course(vec![vec![100, 200], vec![200, 1300], vec![1000, 1250], vec![2000, 3200]]), 3);
+    assert_eq!(
+        Solution::schedule_course(vec![
+            vec![100, 200],
+            vec![200, 1300],
+            vec![1000, 1250],
+            vec![2000, 3200]
+        ]),
+        3
+    );
     assert_eq!(Solution::schedule_course(vec![vec![1, 2]]), 1);
     assert_eq!(Solution::schedule_course(vec![vec![3, 2], vec![4, 3]]), 0);
 }
