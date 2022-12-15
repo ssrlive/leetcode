fe1708e41fa7fdb98df481606f267e6952678a2e
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-12-15 17:30:08 +0800

    minor

--- src/code0771.rs
+++ src/code0771.rs
@@ -41,6 +41,9 @@ impl Solution {
 
 #[test]
 fn test() {
-    assert_eq!(Solution::num_jewels_in_stones("aA".to_string(), "aAAbbbb".to_string()), 3);
+    assert_eq!(
+        Solution::num_jewels_in_stones("aA".to_string(), "aAAbbbb".to_string()),
+        3
+    );
     assert_eq!(Solution::num_jewels_in_stones("z".to_string(), "ZZ".to_string()), 0);
 }
