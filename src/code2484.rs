abab94d705d4c9ee2f7cd2a75819189e48d1a17a
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-12-18 22:49:50 +0800

    minor.

--- src/code2484.rs
+++ src/code2484.rs
@@ -37,41 +37,6 @@
 // - s consists of digits.
 //
 
-/*
-// java solution
-
-class Solution {
-    public int countPalindromes(String s) {
-        final int PALINDROME_LENGTH = 5;
-        final int MODULO = (int) Math.pow(10, 9) + 7;
-
-        long totalPalindromesWithLengthFive = 0;
-
-        for (int first = 0; first <= 9; ++first) {
-            for (int second = 0; second <= 9; ++second) {
-
-                int[] palindrome = {first, second, 0, second, first};
-                long[] memo = {0, 0, 0, 0, 0, 1};
-
-                for (int i = 0; i < s.length(); ++i) {
-                    for (int n = 0; n < PALINDROME_LENGTH; ++n) {
-
-                        int digit = s.charAt(i) - '0';
-                        if (digit == palindrome[n] || n == 2) {
-                            memo[n] = (memo[n] + memo[n + 1]) % MODULO;
-                        }
-                    }
-                }
-                totalPalindromesWithLengthFive = (totalPalindromesWithLengthFive + memo[0]) % MODULO;
-            }
-        }
-        return (int) totalPalindromesWithLengthFive;
-    }
-}
-
-*/
-
-// rust solution
 struct Solution;
 impl Solution {
     pub fn count_palindromes(s: String) -> i32 {
