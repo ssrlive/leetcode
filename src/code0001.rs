fadebea38073503d4a12409deb5babdd194cc307
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-12-21 20:17:52 +0800

    1. Two Sum

---
+++ src/code0001.rs
@@ -0,0 +1,59 @@
+#![allow(dead_code)]
+
+// 1. Two Sum
+// https://leetcode.com/problems/two-sum/
+// https://leetcode.cn/problems/two-sum/
+//
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//
+// You can return the answer in any order.
+//
+// Example 1:
+//
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+//
+// Example 2:
+//
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+//
+// Example 3:
+//
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+//
+// Constraints:
+//
+// - 2 <= nums.length <= 10^4
+// - -10^9 <= nums[i] <= 10^9
+// - -10^9 <= target <= 10^9
+// - Only one valid answer exists.
+//
+// Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+//
+
+struct Solution;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map = std::collections::HashMap::new();
+        for (i, &num) in nums.iter().enumerate() {
+            if let Some(&j) = map.get(&(target - num)) {
+                return vec![j as i32, i as i32];
+            }
+            map.insert(num, i);
+        }
+        vec![]
+    }
+}
+
+#[test]
+fn test() {
+    assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
+    assert_eq!(Solution::two_sum(vec![3, 2, 4], 6), vec![1, 2]);
+    assert_eq!(Solution::two_sum(vec![3, 3], 6), vec![0, 1]);
+}
