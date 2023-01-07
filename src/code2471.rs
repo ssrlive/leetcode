c3acdb1e0a25c4f41507ab0dc58c99604ce7ba43
ssrlive <30760636+ssrlive@users.noreply.github.com>
2023-01-07 15:52:25 +0800

    minor

--- src/code2471.rs
+++ src/code2471.rs
@@ -49,56 +49,6 @@
 
 use crate::treenode::TreeNode;
 
-/*
-// cpp solution
-
-  int minimumOperations(TreeNode* root) {
-    queue<TreeNode *> q;
-    q.emplace(root);
-    int ans = 0;
-    // vector<int> temp;
-    vector<int> v;
-    while(!q.empty()){
-      v.resize(0);
-      int sz = q.size();
-      for(int i = 0; i < sz; i++){
-        auto node = q.front(); q.pop();
-        v.emplace_back(node->val);
-        if(node->left) q.emplace(node->left);
-        if(node->right) q.emplace(node->right);
-      }
-      ans += minSwaps(v);
-    }
-    return ans;
-  }
-  int minSwaps(vector<int> &arr){
-    int n = arr.size();
-    pair<int, int> arrPos[n];
-    for (int i = 0; i < n; i++){
-      arrPos[i].first = arr[i];
-      arrPos[i].second = i;
-    }
-    sort(arrPos, arrPos + n);
-    vector<bool> vis(n, false);
-    int ans = 0;
-    for (int i = 0; i < n; i++){
-      if (vis[i] || arrPos[i].second == i)
-        continue;
-      int temp = 0;
-      int j = i;
-      while (!vis[j]){
-        vis[j] = 1;
-        j = arrPos[j].second;
-        temp++;
-      }
-      if (temp > 0){
-        ans += (temp - 1);
-      }
-    }
-    return ans;
-  }
-*/
-
 struct Solution;
 
 use std::cell::RefCell;
